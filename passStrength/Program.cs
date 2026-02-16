1f45a47ab85cc9db90ea1fcb83e1bef4005a3bdb
kchr2026 <kchr2026@gmail.com>
2026-02-16 13:38:14 +0100

    Correction

--- passStrength/Program.cs
+++ passStrength/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Somebody
+using System;
 
 public static class Program
 {
@@ -7,8 +8,6 @@ public static class Program
 static void Main(string[] args)
 {
     passStrength pass = new passStrength();
-    
-    //
     String password ="";
     //Sjekker om passordet er lang not eller inneholder symboler
     while (!pass.pLength(password.Length) || !pass.pSymbols(password))
